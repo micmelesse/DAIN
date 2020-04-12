ce16b9bcbe05928bcc02e2696d392fea4ec38e19
Michael Melesse <micmelesse@gmail.com>
2020-04-11 21:19:14 -0700

    add scipts

---
+++ scripts/download_data.sh
@@ -0,0 +1,7 @@
+mkdir MiddleBurySet
+cd MiddleBurySet
+wget http://vision.middlebury.edu/flow/data/comp/zip/other-color-allframes.zip
+unzip other-color-allframes.zip
+wget http://vision.middlebury.edu/flow/data/comp/zip/other-gt-interp.zip
+unzip other-gt-interp.zip
+cd ..
---
+++ scripts/download_models.sh
@@ -0,0 +1,3 @@
+mkdir model_weights
+cd model_weights
+wget http://vllab1.ucmerced.edu/~wenbobao/DAIN/best.pth
