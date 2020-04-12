6e5de310a7be53c46d48db9faf51a031a542035c
Michael Melesse <micmelesse@gmail.com>
2020-04-12 00:27:53 -0700

    update docker run

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,4 +1,4 @@
 PWD=`pwd`
-alias drun="docker run -it --rm --network=host -v $PWD:$PWD -w $PWD" 
-# alias drun="docker run -it --rm --network=host --runtime=nvidia -v $PWD:$PWD -w $PWD"
-drun dain
+# alias drun="docker run -it --rm --network=host -v $PWD:$PWD -w $PWD" 
+alias drun="docker run -it --rm --network=host --runtime=nvidia -v $PWD:$PWD -w $PWD"
+drun pytorch/pytorch
